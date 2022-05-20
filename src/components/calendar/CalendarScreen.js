553c4d839a24268564b62d0e9878098857ed51bc
Santiago <santiago.zibe@gmail.com>
2022-05-20 19:37:15 -0300

    inicializacion del proyecto

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CalendarScreen = () => {
+      return (
+            <h1>CalendarScreen</h1>
+      )
+}
+
+export default CalendarScreen
---
+++ src/router/AppRouter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppRouter = () => {
+      return (
+            <h1>AppRouter</h1>
+
+            
+      )
+}
+
+export default AppRouter
